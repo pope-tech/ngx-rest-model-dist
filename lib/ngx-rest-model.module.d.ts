d3e6509169de888151fe6407d20fe4c6b2081cd9
Matt Moore <me@mattdoescode.com>
2021-05-26 14:56:32 -0600

    Update

--- lib/ngx-rest-model.module.d.ts
+++ lib/ngx-rest-model.module.d.ts
@@ -1,5 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class NgxRestModelModule {
     static forRoot(config: any): ModuleWithProviders<NgxRestModelModule>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDeclaration<NgxRestModelModule, never>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDeclaration<NgxRestModelModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDeclaration<NgxRestModelModule>;
 }
+
 //# sourceMappingURL=ngx-rest-model.module.d.ts.map
